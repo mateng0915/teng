b2e86dba98935581714a03062f2a2883cff7cf5f
shuteng.mt <shuteng.mt@alibaba-inc.com>
2018-12-27 16:42:29 +0800

    test 4

--- api.java
+++ api.java
@@ -2,5 +2,7 @@ public class api {
   public static void main (String args) {
     System.out.println("test rebase");
     System.out.println("test rebase2");
+    System.out.println("test rebase3");
+    System.out.println("test rebase4");
   }
 }
