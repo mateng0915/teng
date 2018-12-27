d1827ebdedbae79978cfbcd1ec30b026822bc02d
shuteng.mt <shuteng.mt@alibaba-inc.com>
2018-12-27 16:40:57 +0800

    test 2

--- api.java
+++ api.java
@@ -1,4 +1,6 @@
 public class api {
- public static void main (String args) {
-}
+  public static void main (String args) {
+    System.out.println("test rebase");
+    System.out.println("test rebase2");
+  }
 }
